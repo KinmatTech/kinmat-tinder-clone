f4ba50e4f1717a5a392cbcadeb709eecec13b866
Okechukwu Ani <mathew.ani@unn.edu.ng>
2023-02-03 18:50:10 +0100

    new commit

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,74 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
-//import axios from "./axios";
+import axios from "./axios";
 
-function TinderCards()  {
-    const [people, setPeople] = useState([
-        
-        {
-            name: "Nelson Mandella",
-            url:
-            "https://cdn.britannica.com/67/75567-050-4EBBE84D/Nelson-Mandela.jpg",
-        },
-        
+function TinderCards() {
+  // initialize with empty array
+  const [people, setPeople] = useState([]);
 
-        {
-            name: "Elon Musk",
-            url:
-            "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
-        },
+  //   const [people, setPeople] = useState([
+  //     {
+  //       name: "Elon Musk",
+  //       url:
+  //         "https://www.biography.com/.image/t_share/MTY2MzU3Nzk2OTM2MjMwNTkx/elon_musk_royal_society.jpg",
+  //     },
+  //     {
+  //       name: "Jeff Bezoz",
+  //       url:
+  //         "https://www.biography.com/.image/t_share/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg",
+  //     },
+  //   ]);
 
-        {
-            name: "Thomas Edison",
-            url:
-            "https://cdn.britannica.com/47/79847-050-A78604A0/Thomas-Alva-Edison.jpg",
-        },
-        
-        {
-            name: "Jeff Bezos",
-            url:
-            
-            "https://cdn.britannica.com/56/199056-050-CCC44482/Jeff-Bezos-2017.jpg",
-        },
+  //   when tinder cards component loads it will run this piece code once, and not run it again
+  // add var to bottom bracket, whenever var name changes it's actually gonna refile this code
+  useEffect(() => {
+    async function fetchData() {
+      // cuz base url already set up in axios.js
+      const req = await axios.get("/tinder/cards");
 
-        {
-            name: "Warren Buffet",
-            url:
-            "https://cdn.britannica.com/89/188789-050-51EA5258/Warren-Buffett.jpg",
-        },  
+      // whatever the request.data comes back us
+      setPeople(req.data);
+    }
 
-    ]);
+    fetchData();
+  }, []);
 
-    const swiped = (direction, nameToDelete) => {
-        console.log("removing: " + nameToDelete);
-        };
+  //   console.log(people);
 
-        const outOfFrame = (name) => {
-            console.log(name + " left the screen!");
-        };
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+    // setLastDirection(direction);
+  };
+
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen!");
+  };
 
   return (
-    <div className='tinderCards'>
-        <div className="tinderCards__cardContainer">
-            {people.map((person) => (
-               <TinderCard
-               className= "swipe"
-               key={person.name}
-               preventSwipe= {["up", "down"]}
-               onSwipe={(dir) => swiped(dir, person.name)}
-               onCardLeftScreen={() => outOfFrame(person.name)}
-                >
-                    <div 
-                    style={{ backgroundImage: `url(${person.url})` }}
-                    className="cards"
-                    >
-                        <h3>{person.name}</h3>
-                    </div>
-                </TinderCard>
-            ))} 
-        </div>
-     </div>
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            noSwipe={(dir) => swiped(dir, person.name)}
+            noCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.imgUrl})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
   );
 }
 
